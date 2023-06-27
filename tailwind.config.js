ddc8575dc6a805485f66c5afa451fcdbd992ef1e
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-06-27 11:43:37 +0600

    theme bug fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  darkMode: false,
   theme: {
     extend: {
       backgroundImage: {
@@ -39,20 +38,6 @@ module.exports = {
           "primary": "#00b0ee",
 
           "secondary": "#3e57d0",
-
-          "accent": "#44c99f",
-
-          "neutral": "#25232e",
-
-          "base-100": "#2c363f",
-
-          "info": "#7088d2",
-
-          "success": "#14804a",
-
-          "warning": "#b07f0c",
-
-          "error": "#e84a7f",
         },
       },
     ],
