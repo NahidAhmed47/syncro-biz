a5768e105833aee18bee24ddb840d347c08e0682
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-06-28 16:22:05 +0600

    about page added

--- src/app/(main-layout)/about/page.js
+++ src/app/(main-layout)/about/page.js
@@ -1,13 +1,16 @@
+import CounterBg from '@/components/CounterBg/CounterBg';
+import HomeAbout from '@/components/HomeAbout/HomeAbout';
 import PageBanner from '@/components/PageBanner/PageBanner';
+import Subscribe from '@/components/Subscribe/Subscribe';
 import React from 'react';
 
 const page = () => {
     return (
         <div>
             <PageBanner title="About Us" route1="Home" route2="About"></PageBanner>
-            <div className='section-container'>
-                
-            </div>
+            <HomeAbout></HomeAbout>
+            <CounterBg></CounterBg>
+            <Subscribe></Subscribe>
         </div>
     );
 };
