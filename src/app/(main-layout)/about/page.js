d380063dff4fb4debd14667f761ec75e76f58e51
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-27 11:24:46 +0600

    code optimized

--- src/app/(main-layout)/about/page.js
+++ src/app/(main-layout)/about/page.js
@@ -2,7 +2,6 @@ import CounterBg from '@/components/shared/CounterBg/CounterBg';
 import HomeAbout from '@/components/pages/home/HomeAbout/HomeAbout';
 import PageBanner from '@/components/shared/PageBanner/PageBanner';
 import Subscribe from '@/components/shared/Subscribe/Subscribe';
-import React from 'react';
 
 const AboutPage = () => {
     return (
